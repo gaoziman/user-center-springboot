f5198dce20bb9a1e305f43e2a35d50687c60c164
gaoziman <2942894660@qq.com>
2024-03-22 15:56:40 +0800

    添加删除接口以及管理权限限制

--- src/main/java/org/javatop/usercenter/controller/UserController.java
+++ src/main/java/org/javatop/usercenter/controller/UserController.java
@@ -1,18 +1,23 @@
 package org.javatop.usercenter.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.javatop.usercenter.domian.User;
 import org.javatop.usercenter.domian.dto.UserDto;
 import org.javatop.usercenter.domian.vo.UserVo;
 import org.javatop.usercenter.service.UserService;
-import org.javatop.usercenter.service.impl.UserServiceImpl;
 import org.springframework.web.bind.annotation.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.UnknownFormatConversionException;
+
+import static org.javatop.usercenter.constant.UserConstant.ADMIN_ROLE;
+import static org.javatop.usercenter.constant.UserConstant.USER_LOGIN_STATE;
 
 /**
  * (user)表控制层
@@ -21,6 +26,7 @@ import java.util.UnknownFormatConversionException;
  */
 @RestController
 @RequestMapping("/user")
+@Slf4j
 public class UserController {
     @Autowired
     private UserService userService;
@@ -56,7 +62,7 @@ public class UserController {
      * @return 登录成功-返回用户信息，登录失败-抛出异常
      */
     @PostMapping("/login")
-    public UserDto login(@RequestBody UserVo userVo, HttpServletRequest request) {
+    public UserDto login(@RequestBody UserVo userVo, HttpServletRequest request){
         if (userVo == null){
             return null;
         }
@@ -69,4 +75,54 @@ public class UserController {
         return userService.login(userAccount,userPassword,request);
     }
 
+
+    /**
+     * 用户查询
+     * @param username 用户名
+     * @param request 请求
+     * @return
+     */
+    @GetMapping("/search")
+    public List<User> searchUser(String username,HttpServletRequest request) {
+        if (!isAdmin(request)) {
+            return new ArrayList<>();
+        }
+        QueryWrapper<User> wrapper = new QueryWrapper<>();
+        if (StringUtils.isNotBlank(username)) {
+            wrapper.like("username", username);
+        }
+        return userService.list(wrapper);
+    }
+
+
+    /**
+     * 删除用户
+     * @param id 用户id
+     * @param request 请求
+     * @return
+     */
+    @PostMapping("/delete/{id}")
+    public boolean deleteUser(@PathVariable Long id,HttpServletRequest request) {
+        if (!isAdmin(request)) {
+            return false;
+        }
+        // 判断用户id是否合法
+        if (id <= 0) {
+            log.info("用户id不合法");
+            return false;
+        }
+        return userService.removeById(id);
+    }
+
+
+    /**
+     * 判断用户是否为管理员
+     * @param request 请求
+     * @return true-是管理员，false-不是管理员
+     */
+    private boolean isAdmin(HttpServletRequest request) {
+        // 判断是否为管理员
+        UserDto userDto = (UserDto) request.getSession().getAttribute(USER_LOGIN_STATE);
+        return userDto != null && userDto.getUserRole() == ADMIN_ROLE;
+    }
 }
